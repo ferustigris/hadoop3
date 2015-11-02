cb7e0f962f40223e85f32474d8878fb88fd1fa86
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2015-11-02 22:45:08 +0300

    Line is parsed

--- src/main/java/IPMapper.java
+++ src/main/java/IPMapper.java
@@ -6,19 +6,31 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IPMapper extends MapReduceBase implements
         org.apache.hadoop.mapred.Mapper<LongWritable, Text, Text, IntWritable> {
 
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> out, Reporter reporter) throws IOException {
         String line = value.toString();
-        String[] fields = line.split("\\s");
-
-        System.out.println("fields = " + fields.length);
-        for(String s: fields) {
-            System.out.println(s);
+        line = line.replaceAll("\\[|\\]", "\"");
+        ArrayList<String> fields = new ArrayList<String>();
+        Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(line);
+        while (m.find()) {
+            fields.add(m.group(1));
         }
 
-        out.collect(new Text(fields[0]), new IntWritable(1));
+        if (fields.size() < 7) {
+            System.out.println("Bad data!");
+            System.out.println("fields = " + fields.size());
+            for(String s: fields) {
+                System.out.println(s);
+            }
+            return;
+        }
+
+        out.collect(new Text(fields.get(0)), new IntWritable(Integer.valueOf(fields.get(6))));
     }
 }
